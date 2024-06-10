22d0583f5ae06e15c5daf02b6468a64e413edff5
matilda002 <matilda0817@hotmail.com>
2024-06-10 17:05:38 +0200

    clean-up of code
    added the js-code directly into the html
    instead of having seperate files. Reason
    is to free up clutter.

--- JensensWebApp/matilda-exercises/JavaScript/javascript-seven.js
+++
@@ -1,7 +0,0 @@
-let count = 0;
-const button = document.getElementById("button");
-let countDisplay = document.getElementById("count");
-button.addEventListener("click", function() {
-    count++;
-    countDisplay.innerHTML = count;
-});
